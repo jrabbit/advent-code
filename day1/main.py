b3ef99b75f13b7df287a300fbdbc9bcc7bc9e775
jrabbit <jackjrabbit@gmail.com>
2015-12-02 20:55:58 -0500

    solution to #1 part 2

--- day1/main.py
+++ day1/main.py
@@ -3,6 +3,18 @@
 def main(data):
     print len([x for x in data if x == "("]) - len([x for x in data if x == ")"])
 
+def part2(data):
+    count = 0
+    for position, x in enumerate(data):
+        if x == "(":
+            count += 1
+        else:
+            count -= 1
+        if count < 0:
+            print position +1
+            break
+
 if __name__ == '__main__':
     data = open("input.txt").read()
     main(data)
+    part2(data)
