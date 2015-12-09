08caabad647e8f99506ea68c1611e81dec1594f2
jrabbit <jackjrabbit@gmail.com>
2015-12-09 16:59:29 -0500

    make testable

--- day7/main.py
+++ day7/main.py
@@ -2,18 +2,28 @@ class Instruction(object):
     pass
 
 
-def main(data):
+def main(data, key="a"):
     # [{q.split('->')} for q in data]
     # f = []
     # for instruction in data:
     x = [q.split('->') for q in data]
     p = {q[-1].strip(): q[0] for q in x}
-    start = p['a']
+    start = p[key]
     def f(key):
+        if key.isdigit():
+            print "got value 1", key
+            return int(key)
+        if len(key) > 1:
+            instr = key.split()
+            for i in instr:
+                if i.islower():
+                    z = f(i)
+                    print z
+                    return z
         x = p[key].strip()
         print x
         f(x)
-    f("a")
+    f(key)
 
 
 if __name__ == '__main__':
