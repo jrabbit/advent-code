0d77e72ba78709027ca9e56cd17806fb8ea61a6d
jrabbit <jackjrabbit@gmail.com>
2015-12-12 00:58:45 -0500

    heyo encoder settings ftw

--- day12/main.py
+++ day12/main.py
@@ -1,13 +1,21 @@
 import json
+import collections
 from compiler.ast import flatten
+# This is technically deprecated wubba lubba dub dub
 
 def hook(item):
     inp = item.values()
     return inp
 
-def main(data):
+def hook2(item):
+    inp = item.values()
+    if "red" in inp:
+        return None
+    return inp
+
+def main(data, h=hook):
     count = []
-    for item in flatten(json.loads(data, object_hook=hook)):
+    for item in flatten(json.loads(data, object_hook=h)):
         if isinstance(item, int):
             count.append(item)
     return sum(count)
@@ -16,3 +24,4 @@ def main(data):
 if __name__ == '__main__':
     data = open("input.txt").read()
     print(main(data))
+    print(main(data, hook2))
