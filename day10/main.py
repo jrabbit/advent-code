81e26027b5ff5374cdb164eec58cf3c31cd7ba98
jrabbit <jackjrabbit@gmail.com>
2015-12-12 12:19:38 -0500

    speedups and part2 Save the regex, don't print debugging statements

--- day10/main.py
+++ day10/main.py
@@ -1,8 +1,9 @@
 import re
+PATTERN = re.compile(ur'((\d)(?:\2+)?)') #thanks #regex
+
 
 def breakdown(s):
-    p = re.compile(ur'((\d)(?:\2+)?)') #thanks #regex
-    return [x[0] for x in re.findall(p, s)]
+    return [x[0] for x in re.findall(PATTERN, s)]
 
 def str_count(i, count):
     return str(str(i).count(str(count)))
@@ -12,18 +13,19 @@ def main(start="1113222113", steps=40):
     while steps > 0:
         def f(current):
             ret =  ""
-            print "got current", current
+            # print "got current", current
             for group in breakdown(current):
                 comp = str(len(group))+group[0]
-                print "comp", comp
+                # print "comp", comp
                 ret += comp
             return ret
         # current = str_count(current,count)+current
         current = f(current)
-        print current
+        # print current
         steps -= 1
     return len(current)
 
 
 if __name__ == '__main__':
     print(main())
+    print(main(steps=50))
