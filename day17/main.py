e15e52d1771180a26b2d1246c84062d0eff4b6f7
jrabbit <jackjrabbit@gmail.com>
2015-12-17 12:13:35 -0500

    day 17 part 2

--- day17/main.py
+++ day17/main.py
@@ -1,13 +1,19 @@
 import itertools
 
-def main(data):
+def main(data, target=150):
     data = map(int, data)
     possibilities = []
     for r in range(len(data)):
         possibilities += [x for x in itertools.combinations(data, r)]
-    return len([x for x in itertools.ifilter(lambda x: sum(x) == 150, possibilities)])
+    return [x for x in itertools.ifilter(lambda x: sum(x) == target, possibilities)]
+
+def part2(data, target=150):
+    l = main(data, target)
+    z = len(l[0])
+    return filter(lambda x: len(x) == z, l)
 
 if __name__ == '__main__':
     with open("input.txt") as f:
         data = f.readlines()
-    print(main(data))
+    print(len(main(data)))
+    print(len(part2(data)))
